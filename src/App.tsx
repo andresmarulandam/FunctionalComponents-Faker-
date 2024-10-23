94e845b3512d8612d4fa5039ae2809b0212cde1c
Andres M <marulandaandres19@gmail.com>
2024-10-22 23:17:52 -0500

    CardStudent Component +Faker

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import { faker } from '@faker-js/faker';
+
 import './App.css';
 import CardStudent from './components/CardStudent';
 
 function App() {
   return (
     <>
-      <h1>Students Enrolled</h1>
-      <CardStudent fullname="Andres Marulanda" programmingExperience={3} />
+      <section
+        style={{
+          padding: '20px',
+          maxWidth: '40%',
+          margin: '0 auto',
+          borderRadius: '10px',
+        }}
+      >
+        <h1 style={{ textAlign: 'center' }}>Students Enrolled</h1>
+        <CardStudent
+          fullname={faker.person.fullName()}
+          programmingExperience={faker.number.int({ min: 0, max: 30 })}
+        />
+        <CardStudent
+          fullname={faker.person.fullName()}
+          programmingExperience={faker.number.int({ min: 0, max: 30 })}
+        />
+        <CardStudent
+          fullname={faker.person.fullName()}
+          programmingExperience={faker.number.int({ min: 0, max: 30 })}
+        />
+      </section>
     </>
   );
 }
--- src/components/CardStudent.tsx
+++ src/components/CardStudent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { faker } from '@faker-js/faker';
 
 interface Student {
   fullname: string;
@@ -10,25 +11,54 @@ const CardStudent: React.FC<Student> = ({
   programmingExperience,
 }) => {
   return (
-    <div style={{ display: 'flex', gap: 30 }}>
-      <img
-        src={`https://ui-avatars.com/api/?name=${fullname}`}
-        className="w-100"
-        alt={`Avatar de ${fullname}`}
-      />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
+        border: '1px solid #ccc',
+        margin: '15px',
+        borderRadius: '10px',
+        padding: '10px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // Sombra sutil para dar profundidad
+        transition: 'transform 0.2s',
+      }}
+    >
       <div
         style={{
-          margin: '5px',
           display: 'flex',
-          flexDirection: 'column',
           alignItems: 'center',
+          gap: '10px',
         }}
       >
-        <h2>{fullname}</h2>
-        <h2>
-          Coding Experience:
-          {programmingExperience} años
-        </h2>
+        <img
+          src={faker.image.avatar()}
+          style={{
+            width: '80px',
+            height: '80px',
+            borderRadius: '50%', // Imagen circular
+            border: '2px solid #007BFF', // Borde azul para la imagen
+            objectFit: 'cover', // Asegura que la imagen se recorte adecuadamente
+          }}
+          alt={`Avatar de ${fullname}`}
+        />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            color: '#ccc',
+          }}
+        >
+          <h2 style={{ fontSize: '24px', color: '#007BFF', margin: '0' }}>
+            {fullname}
+          </h2>
+          <h2 style={{ fontSize: '18px', color: '#666', margin: ' 0' }}>
+            Coding Experience: {programmingExperience} años
+          </h2>
+        </div>
+      </div>
+      <div>
+        <h1>parte 2</h1>
       </div>
     </div>
   );
